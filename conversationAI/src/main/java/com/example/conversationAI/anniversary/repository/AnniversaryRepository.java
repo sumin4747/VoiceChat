78d60afbc64b6ce587b065b5e4c0ba30227595c9
LeeYejin013 <remitjoy0413@gmail.com>
2026-04-06 13:36:35 +0900

    feat: 기념일 조회 메소드 추가

--- conversationAI/src/main/java/com/example/conversationAI/anniversary/repository/AnniversaryRepository.java
+++ conversationAI/src/main/java/com/example/conversationAI/anniversary/repository/AnniversaryRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.time.LocalDate;
 
 public interface AnniversaryRepository extends JpaRepository<Anniversary, Long> {
 
@@ -13,4 +16,11 @@ public interface AnniversaryRepository extends JpaRepository<Anniversary, Long> 
     List<Anniversary> findAllByUserIdAndPersonaIdOrderByEventDateAsc(Long userId, Long personaId);
 
     Optional<Anniversary> findByIdAndUserId(Long id, Long userId);
+
+    @Query("SELECT a FROM Anniversary a WHERE a.enabled = true " +
+            "AND ((a.repeatYearly = true " +
+            "AND FUNCTION('MONTH', a.eventDate) = FUNCTION('MONTH', :today) " +
+            "AND FUNCTION('DAY', a.eventDate) = FUNCTION('DAY', :today)) " +
+            "OR (a.repeatYearly = false AND a.eventDate = :today))")
+    List<Anniversary> findTodayAnniversaries(@Param("today") LocalDate today);
 }
